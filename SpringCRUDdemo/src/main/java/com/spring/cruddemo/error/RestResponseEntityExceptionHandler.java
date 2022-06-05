d02fc427d016ca9008b06446df6e655f0a121c3e
yahoosam <yahoosam@gmail.com>
2022-06-05 18:50:38 -0400

    Update RestResponseEntityExceptionHandler.java

--- SpringCRUDdemo/src/main/java/com/spring/cruddemo/error/RestResponseEntityExceptionHandler.java
+++ SpringCRUDdemo/src/main/java/com/spring/cruddemo/error/RestResponseEntityExceptionHandler.java
@@ -13,7 +13,7 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ResponseStatus
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(DepartmentNotFoundException.class)
-    public ResponseEntity <ErrorMessage> departmentNotFoundException(DepartmentNotFoundException exception, WebRequest request){
+    public ResponseEntity<ErrorMessage> departmentNotFoundException(DepartmentNotFoundException exception, WebRequest request) {
         ErrorMessage message = new ErrorMessage(HttpStatus.NOT_FOUND, exception.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(message);
     }
